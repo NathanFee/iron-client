8f711b217d6a60b19d03d3e0b7b9b3be6d1560af
NathanFee <Nathan_Fee@yahoo.com>
2019-03-12 09:45:35 -0400

    Adds auth file from previous Project
    - Adds Auth file
    - Adds handlers for auth forms
    - Links auth file with heroku api

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,16 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events.js')
 
 $(() => {
-  // your JS code goes here
+  // authorization
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#sign-out-link').on('click', authEvents.onSignOut)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+
+  $('.sign-in-button').on('click', authEvents.onSignInButton)
+  $('.sign-up-button').on('click', authEvents.onSignUpButton)
+  $('.close-password-modal').on('click', authEvents.onClosePasswordModal)
 })
